3af145dcd3a083cfd77114c6fefcd720e8f96040
Greg Z <ziebold@gmail.com>
2014-06-20 22:10:01 -0600

    dynamically change box positions and item colors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('\'Allo \'Allo!');
+/*jshint indent:2 */
+'use strict';
+$(document).ready(function() {
+  var buttons = $('.nav-pills > li');
+
+  buttons.click(function() {
+
+    var index = buttons.index(this),
+      content = $('.content'),
+      items = $('.item-box .item');
+
+    // change position of content
+    content.attr('class', function(ignore, cls) {
+      // update class based on button index
+      return cls.replace(/\bposition-\S+/g, 'position-' + index);
+    });
+
+    // reset item colors
+    if (index === 1) {
+      // if two, blue boxes
+      items.removeClass('red').addClass('blue');
+    } else {
+      // otherwise, red boxes
+      items.removeClass('blue').addClass('red');
+    }
+
+  });
+});
