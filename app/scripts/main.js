ff1a5ef7900b9a56395200a7fcf136b43e0b5b83
Greg Z <ziebold@gmail.com>
2014-06-21 19:44:48 -0600

    add history support

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,57 @@
 /*jshint indent:2 */
-/* global AppHelper */
+/* global AppHelper, History */
 'use strict';
 
 $(document).ready(function() {
-  var buttons = $('.nav-pills > li'),
-    appHelper = new AppHelper();
+  var appHelper = new AppHelper(),
+    buttons = $('.nav-pills > li'),
+    titlePrefix = 'StrongLoop UI Eval | ',
 
+    updatePage = function(num) {
+      var content = $('.content'),
+        items = $('.item-box .item');
+
+      // change position of content
+      content.removeClass(appHelper.getOppositeLayouts(num));
+      content.addClass(appHelper.getPageLayout(num));
+
+      // reset item colors
+      var newColor = appHelper.getItemColor(num);
+      items.removeClass(appHelper.getOppositeColor(newColor));
+      items.addClass(newColor);
+
+      // toggle button state
+      buttons.each(function(index, button) {
+        if (index === num - 1) {
+          $(button).addClass('active');
+        } else {
+          $(button).removeClass('active');
+        }
+      });
+    };
+
+  // handle initial load
+  var matches = $(location).attr('href').match(/\/\?([123]{1})/);
+  if (matches === null) {
+    // if we get a bad num, default to one
+    History.replaceState({ 'pageNum': 1 }, titlePrefix + appHelper.getPageName(1), '?1' );
+  } else {
+    updatePage(parseInt(matches[1]));
+  }
+
+  // handle button clicks
   buttons.click(function() {
+    var next = buttons.index(this) + 1;
 
-    var index = buttons.index(this) +1,
-      content = $('.content'),
-      items = $('.item-box .item');
-
-    // change position of content
-    content.removeClass(appHelper.getOppositeLayouts(index));
-    content.addClass(appHelper.getPageLayout(index));
-
-    // reset item colors
-    var newColor = appHelper.getItemColor(index);
-    items.removeClass(appHelper.getOppositeColor(newColor));
-    items.addClass(newColor);
-
+    // update history, will updatePage
+    History.pushState({ 'pageNum': next }, titlePrefix + appHelper.getPageName(next), '?' + next);
   });
+
+  // handle history state changes
+  History.Adapter.bind(window, 'statechange', function() {
+    var state = History.getState();
+    updatePage(state.data.pageNum);
+  });
+
 });
+
